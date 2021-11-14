17c117b9419d879cd78c76aee235e725c9d1718a
Antone Meteorite <ameteori@mi-q1.msk.21-school.ru>
2021-11-14 17:13:50 +0300

    bon

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ameteori <ameteori@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 15:56:51 by ameteori          #+#    #+#             */
-/*   Updated: 2021/11/14 17:00:16 by ameteori         ###   ########.fr       */
+/*   Updated: 2021/11/14 17:04:35 by ameteori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,16 +43,16 @@ char	*read_ostatok(int fd, char *ostatok)
 
 char	*get_next_line(int fd)
 {
-	static char		*ostatok;
+	static char		*ostatok[4096];
 	char			*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	ostatok = read_ostatok(fd, ostatok);
-	if (!ostatok)
+	ostatok[fd] = read_ostatok(fd, ostatok[fd]);
+	if (!ostatok[fd])
 		return (NULL);
-	line = ft_dup_line(ostatok);
-	ostatok = new_ost(ostatok);
+	line = ft_dup_line(ostatok[fd]);
+	ostatok[fd] = new_ost(ostatok[fd]);
 	return (line);
 }
 
